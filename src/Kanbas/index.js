078588a81f8eb9231dff88f99351e63e79d48880
Anant Moudgalya <anant9798@gmail.com>
2023-10-15 20:07:36 -0700

    - fix styling

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -2,7 +2,7 @@ import Nav from "../Nav";
 
 function Kanbas() {
   return (
-    <div>
+    <div className="container">
       <Nav />
       <h1>Kanbas</h1>
     </div>
--- src/Labs/a3/HelloWorld.js
+++ src/Labs/a3/HelloWorld.js
@@ -2,10 +2,10 @@ import Nav from "../../Nav";
 
 function HelloWorld() {
   return (
-    <>
+    <div className="container">
       <Nav />
       <h1>Hello World!</h1>
-    </>
+    </div>
   );
 }
 export default HelloWorld;
