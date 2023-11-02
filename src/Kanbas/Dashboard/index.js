37785c724bcdb4947969e1a3bdcdf451b12262b9
Anant Moudgalya <anant9798@gmail.com>
2023-11-01 19:47:17 -0700

    updated dashboard and courses

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -5,22 +5,57 @@ import "../styles/theme.css";
 
 const { courses } = db;
 
-function Dashboard() {
+function Dashboard({ courses, course, setCourse, addNewCourse, deleteCourse, updateCourse }) {
   return (
-    <div className="kanbas-column" style={{ width: "100%" }}>
+    <div className="kanbas-column" style={{ width: "75%" }}>
       <h1>Dashboard</h1>
       <hr />
+
+      <div className="mb-2">
+        <h5>Course Modification</h5>
+        <input
+          value={course.name}
+          className="form-control mb-2"
+          onChange={(e) => setCourse({ ...course, name: e.target.value })}
+        />
+        <input
+          value={course.number}
+          className="form-control mb-2"
+          onChange={(e) => setCourse({ ...course, number: e.target.value })}
+        />
+        <input
+          value={course.startDate}
+          className="form-control mb-2"
+          type="date"
+          onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+        />
+        <input
+          value={course.endDate}
+          className="form-control mb-2"
+          type="date"
+          onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+        />
+
+        <button onClick={addNewCourse} className="btn btn-success mb-2 me-2">
+          Add
+        </button>
+
+        <button onClick={updateCourse} className="btn btn-primary mb-2 me-2">
+          Update
+        </button>
+      </div>
+
       <h2>Published Courses ({courses.length})</h2>
       <hr />
       <div className="d-flex flex-row flex-wrap">
-        {courses.map((course) => (
+        {courses.map((course, index) => (
           <a
             href={`../#/Kanbas/Courses/${course._id}`}
             className="kanbas-card"
             key={course._id}
           >
             <div className="card">
-              <div className={`card-header bg-${randomColorGenerator(course._id)}`}>
+              <div className={`card-header bg-${randomColorGenerator(index)}`}>
                 <i className="fa fa-ellipsis-vertical float-end kanbas-ellipsis"></i>
                 <br />
                 <br />
@@ -33,6 +68,24 @@ function Dashboard() {
                 <h6 className="card-title">{`${course.number}.${course._id}`}</h6>
                 <p className="card-text">{getCourseTerm(course.startDate)}</p>
               </div>
+              <button
+                className="btn btn-warning ms-2 me-2 mb-2"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setCourse(course);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="btn btn-danger ms-2 me-2 mb-2"
+                onClick={(event) => {
+                  event.preventDefault();
+                  deleteCourse(course._id);
+                }}
+              >
+                Delete
+              </button>
             </div>
           </a>
         ))}
@@ -63,7 +116,7 @@ function getCourseTerm(startDate) {
   return "";
 }
 
-function randomColorGenerator() {
+function randomColorGenerator(index) {
   const colors = [
     "primary",
     "secondary",
@@ -72,8 +125,7 @@ function randomColorGenerator() {
     "warning",
     "info",
   ];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  const selectedColor = colors[randomIndex];
+  const selectedColor = colors[index % colors.length];
   return selectedColor;
 }
 
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -3,19 +3,66 @@ import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
+import { useState } from "react";
+import db from "./Database";
 
 function Kanbas() {
-    return (
-        <div className="d-flex">
-            <KanbasNavigation/>
-            <div>
-                <Routes>
-                    <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route path="Dashboard" element={<Dashboard />} />
-                    <Route path="Courses/:courseId/*" element={<Courses />} />
-                </Routes>
-            </div>
-        </div>
+  const [courses, setCourses] = useState(db.courses);
+
+  const [course, setCourse] = useState({
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+  });
+
+  const addNewCourse = () => {
+    const randomId = `RS11${Math.floor(Math.random() * 10)}`;
+    setCourses([...courses, { ...course, _id: randomId }]);
+  };
+
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
     );
+  };
+
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route
+            path="Dashboard"
+            element={
+              <Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}
+              />
+            }
+          />
+          <Route
+            path="Courses/:courseId/*"
+            element={<Courses courses={courses} />}
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 export default Kanbas;
