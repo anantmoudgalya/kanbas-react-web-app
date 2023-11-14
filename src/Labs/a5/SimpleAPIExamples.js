4044133dc1adae7e9088d9edf412b9a860650879
Anant Moudgalya <anant9798@gmail.com>
2023-11-13 22:50:07 -0800

    - finish 3.4

--- src/Labs/a5/SimpleAPIExamples.js
+++ src/Labs/a5/SimpleAPIExamples.js
@@ -1,11 +1,15 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjects from "./WorkingWithObjects";
 
 function SimpleAPIExamples() {
   return (
     <>
       <EncodingParametersInURLs />
+      <br/><br/>
       <WorkingWithObjects />
+      <br/><br/>
+      <WorkingWithArrays />
     </>
   );
 }
