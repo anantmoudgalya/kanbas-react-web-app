524adea0dbcd35872086a6557e91d9a9564c287d
Anant Moudgalya <anant9798@gmail.com>
2023-12-01 22:44:26 -0800

    - code cleanup

--- src/Kanbas/Users/SignIn.js
+++ src/Kanbas/Users/SignIn.js
@@ -1,40 +1,48 @@
 import * as client from "./client";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 function SignIn() {
   const [credentials, setCredentials] = useState({
     username: "",
     password: "",
   });
   const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
   const signin = async () => {
     await client.signin(credentials);
     navigate("/Kanbas/account");
   };
+
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Sign In</h1>
       <input
+        type="text"
+        name="username"
         value={credentials.username}
-        placeholder="username"
+        placeholder="Username"
         className="form-control mb-2"
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
+        onChange={handleInputChange}
       />
       <input
+        type="password"
+        name="password"
         value={credentials.password}
-        placeholder="password"
+        placeholder="Password"
         className="form-control mb-2"
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
+        onChange={handleInputChange}
       />
       <button onClick={signin} className="btn btn-success mb-2 me-2">
-        {" "}
-        Signin{" "}
+        Sign In
       </button>
     </div>
   );
 }
+
 export default SignIn;
